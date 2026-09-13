50cd1a4b10ca5cf54f151f5164fab7ad5817fd84
priyanshu101120 <gs093863@gmail.com>
2026-09-13 09:52:59 +0530

    feat: optimize performance

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,76 +1,76 @@
-// import { useRouter } from "next/navigation";
-// import { useEffect, useState } from "react";
-// import { SafeUser, LoginInput } from "./type";
-// import { authApi } from "@/lib/api";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { SafeUser, LoginInput } from "./type";
+import { authApi } from "@/lib/api";
 
-// const useAuth = () => {
-//   const [user, setUser] = useState<SafeUser | null>(null);
-//   const [loading, setLoading] = useState(true);
-//   const router = useRouter();
+const useAuth = () => {
+  const [user, setUser] = useState<SafeUser | null>(null);
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
 
-//   useEffect(() => {
-//     let isMounted = true;
+  useEffect(() => {
+    let isMounted = true;
 
-//     const fetchUser = async () => {
-//       try {
-//         const  data  = await authApi.getMe();
-//         if (isMounted) setUser(data.user);
-//         setLoading(false);
-//       } catch (error) {
-//         if (isMounted) setUser(null);
-//       } finally {
-//         if (isMounted) setLoading(false);
-//       }
-//     };
-//     fetchUser();
-//     return () => {
-//       isMounted = false;
-//     };
-//   }, []);
+    const fetchUser = async () => {
+      try {
+        const  data  = await authApi.getMe();
+        if (isMounted) setUser(data.user);
+        setLoading(false);
+      } catch (error) {
+        if (isMounted) setUser(null);
+      } finally {
+        if (isMounted) setLoading(false);
+      }
+    };
+    fetchUser();
+    return () => {
+      isMounted = false;
+    };
+  }, []);
 
-//   const login = async (email: string, password: string): Promise<void> => {
-//     // Implementation for login
-//     try {
-//       const data  = await authApi.login({ email, password });
-//       setUser(data.user);
-//     } catch (error) {
-//       const message = error instanceof Error ? error.message : "Login failed";
-//       throw new Error(message);
-//     }
-//   };
+  const login = async (email: string, password: string): Promise<void> => {
+    // Implementation for login
+    try {
+      const data  = await authApi.login({ email, password });
+      setUser(data.user);
+    } catch (error) {
+      const message = error instanceof Error ? error.message : "Login failed";
+      throw new Error(message);
+    }
+  };
 
-//   const register = async (
-//     name: string,
-//     email: string,
-//     password: string,
-//   ): Promise<void> => {
-//     try {
-//       const  data  = await authApi.register({ name, email, password });
-//       setUser(data.user);
-//     } catch (error) {
-//       const message =
-//         error instanceof Error ? error.message : "Registration failed";
-//       throw new Error(message);
-//     }
-//   };
-//   const logOut = async (): Promise<void> => {
-//     try {
-//       await authApi.logOut();
-//       setUser(null);
-//       router.push("/login");
-//     } catch (error) {
-//       const message = error instanceof Error ? error.message : "Logout failed";
-//       throw new Error(message);
-//     }
-//   };
+  const register = async (
+    name: string,
+    email: string,
+    password: string,
+  ): Promise<void> => {
+    try {
+      const  data  = await authApi.register({ name, email, password });
+      setUser(data.user);
+    } catch (error) {
+      const message =
+        error instanceof Error ? error.message : "Registration failed";
+      throw new Error(message);
+    }
+  };
+  const logOut = async (): Promise<void> => {
+    try {
+      await authApi.logout();
+      setUser(null);
+      router.push("/login");
+    } catch (error) {
+      const message = error instanceof Error ? error.message : "Logout failed";
+      throw new Error(message);
+    }
+  };
 
-//   return {
-//     user,
-//     loading,
-//     login,
-//     register,
-//     logOut,
-//   };
-// };
+  return {
+    user,
+    loading,
+    login,
+    register,
+    logOut,
+  };
+};
 
-// export default useAuth;
+export default useAuth;
